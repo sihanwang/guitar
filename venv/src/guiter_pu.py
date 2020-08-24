0680a228ec70777c39e0d41b577bfc4c405bed0a
sihanwang <ocpwj@hotmail.com>
2020-08-24 13:57:56 +0800

    test

--- venv/src/guiter_pu.py
+++ venv/src/guiter_pu.py
@@ -1,10 +1,12 @@
 import random
+import time
 
 pu=['下加三间','下加一间','五线','三线','下加4间','下加一线','四间','下加三线','二间','三间','一线','四线','二线','下加二间','一间','下加二线','上加一间']
 
 qin=['6弦3品','4弦空弦','1弦1品','2弦空弦','6弦空弦','5弦3品','1弦空弦','6弦1品','3弦2品','2弦1品','4弦2品','2弦3品','3弦空弦','5弦2品','4弦3品','5弦空弦','1弦3品']
 
 yin=['低音5','中音2','高音4','中音7','低音3','中音1','高音3','低音4','中音6','高音1','中音3','高音2','中音5','低音7','中音4','低音6','高音5']
+
 
 puMapping={'下加三间':('低音5','6弦3品'),'下加一间':('中音2','4弦空弦'),'五线':('高音4','1弦1品'),'三线':('中音7','2弦空弦'),'下加4间':('低音3','6弦空弦'),'下加一线':('中音1','5弦3品'),
             '四间':('高音3','1弦空弦'),'下加三线':('低音4','6弦1品'),'二间':('中音6','3弦2品'),'三间':('高音1','2弦1品'),'一线':('中音3','4弦2品'),'四线':('高音2','2弦3品'),
@@ -44,13 +46,15 @@ def printQinChoices(answer,number):
         i=i+1
     return qinChoices
 
-
 random.shuffle(pu)
-
+m=0
+starttime=time.time()
 for key in pu:
+    m=m+1
+    questionstarttime=time.time()
     while True:
         print('#####')
-        print(key)
+        print('('+str(m)+')', key)
         print('#####')
         yinChoices=printYinChoices(puMapping[key][0],5)
         print()
@@ -65,11 +69,12 @@ for key in pu:
             qinAnswer=answer[1:2]
             mapping=puMapping[key]
             if yinChoices.get(yinAnswer)==mapping[0] and qinChoices.get(qinAnswer)==mapping[1]:
-                print("Correct! Good job!")
+                questioncompletetime=(time.time()-questionstarttime)//1
+                print('Correct! Good job in '+str(questioncompletetime)+' seconds !')
                 break
             else:
-                print("Incorrect answer!")
+                print('Incorrect answer!')
                 wrongList.add(key)
 
-
-print('Test complete. Pls find wrong list:', wrongList)
+spenttime=(time.time()-starttime)//1
+print('Test complete in '+ str(spenttime) +' seconds, Pls find wrong list:', wrongList)
--- venv/src/guiter_yin.py
+++ venv/src/guiter_yin.py
@@ -1,14 +1,19 @@
 import random
+import time
 
 pu=['下加三间','下加一间','五线','三线','下加4间','下加一线','四间','下加三线','二间','三间','一线','四线','二线','下加二间','一间','下加二线','上加一间']
 
 qin=['6弦3品','4弦空弦','1弦1品','2弦空弦','6弦空弦','5弦3品','1弦空弦','6弦1品','3弦2品','2弦1品','4弦2品','2弦3品','3弦空弦','5弦2品','4弦3品','5弦空弦','1弦3品']
 
+'''
 yin=['低音5','中音2','高音4','中音7','低音3','中音1','高音3','低音4','中音6','高音1','中音3','高音2','中音5','低音7','中音4','低音6','高音5']
+'''
+yin=['低音5','中音2']
 
 yinMapping={'低音5':('下加三间','6弦3品'),'中音2':('下加一间','4弦空弦'),'高音4':('五线','1弦1品'),'中音7':('三线','2弦空弦'),'低音3':('下加4间','6弦空弦'),'中音1':('下加一线','5弦3品'),
             '高音3':('四间','1弦空弦'),'低音4':('下加三线','6弦1品'),'中音6':('二间','3弦2品'),'高音1':('三间','2弦1品'),'中音3':('一线','4弦2品'),'高音2':('四线','2弦3品'),
             '中音5':('二线','3弦空弦'),'低音7':('下加二间','5弦2品'),'中音4':('一间','4弦3品'),'低音6':('下加二线','5弦空弦'),'高音5':('上加一间','1弦3品')}
+
 
 wrongList=set([])
 
@@ -45,11 +50,14 @@ def printQinChoices(answer,number):
     return qinChoices
 
 random.shuffle(yin)
-
+m=0
+starttime=time.time()
 for key in yin:
+    m=m+1
+    questionstarttime=time.time()
     while True:
         print('#####')
-        print(key)
+        print('('+str(m)+')', key)
         print('#####')
         puChoices=printPuChoices(yinMapping[key][0],5)
         print()
@@ -64,11 +72,12 @@ for key in yin:
             qinAnswer=answer[1:2]
             mapping=yinMapping[key]
             if puChoices.get(puAnswer)==mapping[0] and qinChoices.get(qinAnswer)==mapping[1]:
-                print("Correct! Good job!")
+                questioncompletetime=(time.time()-questionstarttime)//1
+                print('Correct! Good job in '+str(questioncompletetime)+' seconds !')
                 break
             else:
-                print("Incorrect answer!")
+                print('Incorrect answer!')
                 wrongList.add(key)
 
-
-print('Test complete. Pls find wrong list:', wrongList)
+spenttime=(time.time()-starttime)//1
+print('Test complete in '+ str(spenttime) +' seconds, Pls find wrong list:', wrongList)
